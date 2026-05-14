9d320f4e45f00a3c35f898d5ba0ce09c26ee5896
Artzsa <diaralzsa@gmail.com>
2026-05-14 15:26:20 +0700

    Fix dotenv paths and add vercel config

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 // Koneksi ke Neon/Postgres
 // Gunakan ssl: true karena Neon mewajibkan SSL
